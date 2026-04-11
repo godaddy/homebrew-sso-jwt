417685993113fbfdf8a1d49850e94df6af91dc75
Jay Gowdy <jay@gowdy.me>
2026-04-11 12:36:52 -0700

    Use git source for private repo compatibility

--- Formula/sso-jwt.rb
+++ Formula/sso-jwt.rb
@@ -1,9 +1,9 @@
 class SsoJwt < Formula
   desc "SSO JWT CLI with hardware-backed secure caching (Secure Enclave / TPM)"
   homepage "https://github.com/gdcorp-im/sso-jwt"
-  url "https://github.com/gdcorp-im/sso-jwt/releases/download/v0.1.0/sso-jwt-0.1.0.tar.gz",
-      using: CurlDownloadStrategy
-  sha256 "fbde5482a7efeee1aca7c855cc1d5871a5fb57ab181cc828a4e538ac0a7e9ac9"
+  url "https://github.com/gdcorp-im/sso-jwt.git",
+      tag: "v0.1.0",
+      revision: "8b79f95446b9b7d69159c7a0a5cf382b3a049957"
   license "MIT"
   head "https://github.com/gdcorp-im/sso-jwt.git", branch: "main"
 
